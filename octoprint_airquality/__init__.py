da607347ffa214a300aa67ed15d1e5497d61734f
Daniel Stanton <stringsonfire@me.com>
2020-03-06 20:59:50 +0000

    Display Name

--- octoprint_airquality/__init__.py
+++ octoprint_airquality/__init__.py
@@ -41,7 +41,7 @@ class AirqualityPlugin(octoprint.plugin.SettingsPlugin,
 		# for details.
 		return dict(
 			airquality=dict(
-				displayName="Airquality Plugin",
+				displayName="Air Quality Plugin",
 				displayVersion=self._plugin_version,
 
 				# version check: github repository
@@ -59,7 +59,7 @@ class AirqualityPlugin(octoprint.plugin.SettingsPlugin,
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Airquality Plugin"
+__plugin_name__ = "Air Quality Plugin"
 
 # Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
 # Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
