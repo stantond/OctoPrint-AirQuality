02a64b0b6443ad2dae46a77a66f176dad12ce33d
Daniel Stanton <stringsonfire@me.com>
2020-03-25 16:13:48 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "0.0.1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """TODO"""
+plugin_description = "Stay safe by monitoring the air quality when you're melting plastic."
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Daniel Stanton"
