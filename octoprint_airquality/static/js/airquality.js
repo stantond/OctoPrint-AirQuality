ae687377d69e898a2ea393efd523789ac9b49e24
Daniel Stanton <stringsonfire@me.com>
2020-06-26 20:16:03 +0100

    Fixed issue when editing device with unplugged connection, added warnings for this situation, added logging for  why settings updates without save button

--- octoprint_airquality/static/js/airquality.js
+++ octoprint_airquality/static/js/airquality.js
@@ -22,59 +22,43 @@ $(function() {
         self.selectedDeviceIndex = 0;
         self.unsavedChanges = ko.observable(false);
 
-        self.serialPortsListEdit = ko.computed(function() {
-            console.log("DEBUG: COMPUTING");
-            var fullList = [];
-            fullList.push(...self.serialPortsList());
-            console.log("DEBUG: fullList from serialPortsList:");
-            console.log(fullList);
+        self.portUnavailable = ko.computed(function() {
             if (self.selectedDevice() !== undefined) {
-                // console.log(self.arrDevices()[self.selectedDeviceIndex].port());
-                console.log("DEBUG: selectedDevice() exists...");
-                console.log("DEBUG: selectedDevice() port is ");
-                console.log(self.selectedDevice().port());
-                if (fullList.indexOf(self.selectedDevice().port()) === -1 && self.selectedDevice().port() !== undefined) {
-                    console.log("DEBUG: selectedDevice() port is not in the list anymore:");
-                    console.log(self.selectedDevice().port());
-                    console.log("DEBUG: adding it back:");
-                    fullList.push(self.selectedDevice().port());
-                    console.log(fullList);
+                if (self.serialPortsList.indexOf(self.selectedDevice().port()) == -1 && self.selectedDevice().port() !== undefined) {
+                    return true;
+                } else {
+                    return false;
                 };
-                console.log("DEBUG: selectedDevice() port is now:");
-                console.log(self.selectedDevice().port());
-            } else {
-                console.log("DEBUG: selectedDevice() is undefined.");
             };
-            console.log("DEBUG: fullList final:");
-            console.log(fullList);
-            return fullList.sort();
         });
 
+        self.serialPortsListEdit = ko.computed(function() {
+            var fullList = [];
+            fullList.push(...self.serialPortsList());
+            if (self.selectedDevice() !== undefined) {
+                if (fullList.indexOf(self.selectedDevice().port()) == -1 && self.selectedDevice().port() !== undefined) {
+                    fullList.push(ko.toJS(self.selectedDevice().port()));
+                };
+            };
+            return fullList;
+        });
 
-        self.arrDevices.subscribe(function(newValue) {
-            console.log(newValue);
-            var unsavedDevices = (ko.toJSON(self.arrDevices()) !== ko.toJSON(self.settings.settings.plugins.airquality.arrDevices()));
+        ko.computed(function() {
+            return ko.toJSON(self.arrDevices);
+        }).subscribe(function() {
+            // @TODO find out why settings arrDevices and observable arrDevices are always identical
+            console.log("change to arrDevices detected");
+            console.log("js arrDevices");
             console.log(ko.toJSON(self.arrDevices()));
+            console.log("settings arrDevices");
             console.log(ko.toJSON(self.settings.settings.plugins.airquality.arrDevices()));
+            var unsavedDevices = (ko.toJSON(self.arrDevices()) !== ko.toJSON(self.settings.settings.plugins.airquality.arrDevices()));
             if (unsavedDevices) {
                 self.unsavedChanges(true);
             } else {
                 self.unsavedChanges(false);
             };
-        }, self, "arrayChange");
-
-        // self.serialPortsListEdit = ko.observableArray();
-        // self.selectedDevice.subscribe(function() {
-        //     if (self.selectedDevice() !== undefined) {
-        //         console.log(self.serialPortsList());
-        //         var fullList = ko.utils.arrayMap(self.serialPortsList(), function(item) {
-        //             console.log("DEBUG: Adding item: " + item);
-        //             return item;
-        //         });
-        //     }
-        // });
-
-
+        });
 
         self.models = ko.observableArray(mapDictionaryToArray(self.supportedDevices));
 
@@ -87,11 +71,11 @@ $(function() {
         }
 
         self.onBeforeBinding = function() {
-            self.arrDevices(self.settings.settings.plugins.airquality.arrDevices());
+            self.arrDevices.push(...self.settings.settings.plugins.airquality.arrDevices());
         }
 
         self.onAfterBinding = function() {
-            // TODO handle missing devices
+            // TODO handle missing devices?
         }
 
         self.onDataUpdaterPluginMessage = function(pluginName, message) {
@@ -156,18 +140,17 @@ $(function() {
             })
         }
 
-        // self.onSettingsBeforeSave = function(payload) {
-        //     console.log("To JSON");
-        //     console.log(ko.toJSON(self.arrDevices()));
-        //     console.log(ko.toJSON(self.settings.settings.plugins.airquality.arrDevices()));
-        //     var devices_updated = (ko.toJSON(self.arrDevices()) !== ko.toJSON(self.settings.settings.plugins.airquality.arrDevices()));
-        //     console.log(devices_updated);
-        //     if(devices_updated){
-                // self.settings.settings.plugins.airquality.arrDevices(self.arrDevices());
-                // TODO reload devices
-                // console.log(self.settings.settings.plugins.airquality.arrDevices());
-        //     }
-        // }
+        self.onSettingsBeforeSave = function(payload) {
+            // @TODO Save settings only when the Save button is pressed, and restart the sensor thread
+            var unsavedDevices = (ko.toJSON(self.arrDevices()) !== ko.toJSON(self.settings.settings.plugins.airquality.arrDevices()));
+            console.log("before");
+            console.log(self.settings.settings.plugins.airquality.arrDevices());
+            if (unsavedDevices) {
+                self.settings.settings.plugins.airquality.arrDevices(ko.toJS(self.arrDevices()));
+            };
+            console.log("after");
+            console.log(self.settings.settings.plugins.airquality.arrDevices());
+        }
 
         self.showAddDeviceModal = function() {
             self.selectedDevice({
@@ -179,7 +162,7 @@ $(function() {
             $("#AirQualityDeviceAddModal").modal("show");
         }
 
-        self.addDeviceToSettings = function() {
+        self.addDevice = function() {
             self.arrDevices.push(self.selectedDevice());
         }
 
@@ -195,7 +178,8 @@ $(function() {
             $("#AirQualityDeviceEditModal").modal("show");
         }
 
-        self.applyEditDeviceToSettings = function(device) {
+        self.applyEditDevice = function(device) {
+            // @todo Ensure that port is not in use by multiple devices
             nonObservableDevice = ko.toJS(device);
             self.arrDevices()[self.selectedDeviceIndex].name(nonObservableDevice["name"]);
             self.arrDevices()[self.selectedDeviceIndex].model(nonObservableDevice["model"]);
